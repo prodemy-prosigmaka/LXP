4f7a898b67dd1bbed2ebd596b98930f3615e1582
hairunisa29 <hairunisa2909@outlook.com>
2024-11-15 17:24:14 +0700

    create student seeder

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userIds = User::whereBetween('id', [4, 5])->pluck('id')->toArray();
+        $occupations = ['Frontend Engineer', 'Software Engineer'];
+        $genders = ['Male', 'Female'];
+        $addresses = ['823 Oak St', '754 Pine St'];
+
+        foreach ($userIds as $userId) {
+            DB::table('students')->insert([
+                'user_id'     => $userId,
+                'phonenumber' => '+62' . rand(8000000000, 8999999999),
+                'occupation'  => $occupations[array_rand($occupations)],
+                'gender'      => $genders[array_rand($genders)],
+                'address'     => $addresses[array_rand($addresses)],
+            ]);
+        }
+    }
+}
