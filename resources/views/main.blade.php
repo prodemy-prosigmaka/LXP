1a4c38ec6343a3323955d6d10e38228988c3d04d
AbidKhairyAK <abid.khairy.ak@gmail.com>
2024-11-20 16:19:59 +0700

    feat: set dir structure

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>
+            @yield('title') | {{ config('app.name') }}
+        </title>
+
+        <!-- Fonts -->
+        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        @yield('layout')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('public.homepage');
+    return view('public.homepage.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('admin.dashboard');
+    return view('admin.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
