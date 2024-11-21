90c31cb38dc6b5944179d67282a9265e8a3759e2
hairunisa29 <hairunisa2909@outlook.com>
2024-11-21 10:18:33 +0700

    create header

--- resources/views/public/_layouts/navigation.blade.php
+++ resources/views/public/_layouts/navigation.blade.php
@@ -1,5 +1,13 @@
-<h3>contoh navigation</h3>
-<a href="{{ route('login') }}">
-	to login
-</a>
-<hr>
+<header class="navbar bg-base-100 shadow-md px-4 md:px-20">
+    <a class="flex-1">
+        <img src="{{ asset('assets/logo-prodemy.png') }}" alt="logo" class="w-28">
+    </a>
+    <nav class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+            <li><a class="text-secondary">Sign In</a></li>
+            <li>
+                <a class="btn btn-sm btn-primary">Sign Up</a>
+            </li>
+        </ul>
+    </nav>
+</header>
