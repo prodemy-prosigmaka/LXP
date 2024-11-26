d4a009439041dcf8e80691eb9f9e86b0cad0731b
AbidKhairyAK <abid.khairy.ak@gmail.com>
2024-11-26 19:27:02 +0700

    feat: course list

--- resources/views/admin/_layouts/app.blade.php
+++ resources/views/admin/_layouts/app.blade.php
@@ -6,16 +6,20 @@
 
 		<!-- Page Heading -->
 		@hasSection('header')
-			<header class="bg-white shadow p-4 md:ml-56">
-				<div class="max-w-7xl mx-auto py-7 pt-24 px-4 sm:px-6 lg:px-8">
-					@yield('header')
+			<header class="p-4 md:ml-56">
+				<div class="max-w-7xl mx-auto pt-20 sm:px-6 lg:px-8">
+					<h2 class="font-semibold text-3xl text-gray-800 leading-tight">
+						@yield('header')
+					</h2>
 				</div>
 			</header>
 		@endif
 
 		<!-- Page Content -->
 		<main class="p-4 md:ml-56">
-			@yield('content')
+		    <div class="max-w-full sm:px-6 lg:px-8">
+				@yield('content')
+			</div>
 		</main>
 	</div>
 @endsection
--- resources/views/admin/course/index.blade.php
+++ resources/views/admin/course/index.blade.php
@@ -1,76 +1,51 @@
 @extends('admin._layouts.app')
 
-@section('title', __('Courses'))
+@section('title', 'Course CMS')
 
-@section('header')
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Courses') }}
-    </h2>
-@endsection
+@section('header', 'Course CMS')
 
 @section('content')
-    <div class="py-12">
-        <div class="max-w-full mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="w-full">
-                    <div class="sm:flex sm:items-center">
-                        <div class="sm:flex-auto">
-                            <h1 class="text-base font-semibold leading-6 text-gray-900">{{ __('Courses') }}</h1>
-                            <p class="mt-2 text-sm text-gray-700">A list of all the {{ __('Courses') }}.</p>
-                        </div>
-                        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <a type="button" href="{{ route('courses.create') }}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add new</a>
-                        </div>
-                    </div>
+    <x-admin.page-card
+        title="Courses"
+        caption="A list of all the Courses"
+    >
+        <x-slot:header_action>
+            <a
+                href="{{ route('courses.create') }}"
+                class="btn shadow btn-primary mt-4 md:mt-0 w-full md:w-auto">
+                <x-lucide-file-plus-2 class="w-4 h-4 "/>
+                Add new
+            </a>
+        </x-slot>
 
-                    <div class="flow-root">
-                        <div class="mt-8 overflow-x-auto">
-                            <div class="inline-block min-w-full py-2 align-middle">
-                                <table class="w-full divide-y divide-gray-300">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">No</th>
-                                        
-                                    <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Instructor Id</th>
-                                    <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Title</th>
-                                    <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Caption</th>
-                                    <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Description</th>
 
-                                        <th scope="col" class="px-3 py-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500"></th>
-                                    </tr>
-                                    </thead>
-                                    <tbody class="divide-y divide-gray-200 bg-white">
-                                    @foreach ($courses as $course)
-                                        <tr class="even:bg-gray-50">
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-semibold text-gray-900">{{ ++$i }}</td>
-                                            
-                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $course->instructor_id }}</td>
-                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $course->title }}</td>
-                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $course->caption }}</td>
-                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $course->description }}</td>
+        <x-table.parent class="mt-8">
+            <x-slot:thead>
+                <x-table.th class="min-w-12 w-12"> ID </x-table.th>
+                <x-table.th> Title </x-table.th>
+                <x-table.th class="min-w-36 w-36 sticky right-0"> Action </x-table.th>
+            </x-slot>
 
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900">
-                                                <form action="{{ route('courses.destroy', $course->id) }}" method="POST">
-                                                    <a href="{{ route('courses.show', $course->id) }}" class="text-gray-600 font-bold hover:text-gray-900 mr-2">{{ __('Show') }}</a>
-                                                    <a href="{{ route('courses.edit', $course->id) }}" class="text-indigo-600 font-bold hover:text-indigo-900  mr-2">{{ __('Edit') }}</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <a href="{{ route('courses.destroy', $course->id) }}" class="text-red-600 font-bold hover:text-red-900" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;">{{ __('Delete') }}</a>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
+            <x-slot:tbody>
+                @foreach ($courses as $course)
+                    @php $order = ++$i; @endphp
+                    <x-table.tr>
+                        <x-table.td>{{ $course->id }}</x-table.td>
+                        <x-table.td>{{ $course->title }}</x-table.td>
+                        <x-table.td :sort_order="$order">
+                            <button class="btn btn-sm btn-secondary shadow">
+                                <x-lucide-square-pen class="w-4 h-4 "/>
+                                Detail
+                            </button>
+                        </x-table.td>
+                    </x-table.tr>
+                @endforeach
+            </x-slot>
 
-                                <div class="mt-4 px-4">
-                                    {!! $courses->withQueryString()->links() !!}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <x-slot:pagination>
+                {!! $courses->withQueryString()->links() !!}
+            </x-slot>
+        </x-table.parent>
+
+    </x-admin.page-card>
 @endsection
