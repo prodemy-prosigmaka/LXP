0db4ed37106736d3daf20a285976d637cfe5c30e
AbidKhairyAK <abid.khairy.ak@gmail.com>
2024-11-28 14:56:30 +0700

    fix: tidy up route & controller structure

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,48 @@
 <?php
 
-use App\Http\Controllers\Admin\ChapterController;
-use App\Http\Controllers\Admin\CourseController;
-use App\Http\Controllers\Admin\CourseStudentController;
-use App\Http\Controllers\Admin\InstructorController;
-use App\Http\Controllers\Admin\LessonArticleController;
-use App\Http\Controllers\Admin\LessonController;
-use App\Http\Controllers\Admin\LessonPdfController;
-use App\Http\Controllers\Admin\LessonVideoController;
-use App\Http\Controllers\Admin\PracticeController;
-use App\Http\Controllers\Admin\ProfileController;
-use App\Http\Controllers\Admin\StudentController;
-use App\Http\Controllers\Admin\TopicController;
+// public
 use App\Http\Controllers\Public\MyLearningController;
 use App\Http\Controllers\Public\PublicCourseController;
 
+// admin
+use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProfileController;
+
+// lib
 use Illuminate\Support\Facades\Route;
 
+
+/**
+ * Public Routes
+ */
 route::get("/", [PublicCourseController::class, 'index'])->name('courselist.index');
 route::get("/course/detail/{id}", [PublicCourseController::class, 'show'])->name('courselist.detail');
 
 route::get("/my-learning", [MyLearningController::class, 'index'])->name('mylearning');
 route::post("/my-learning/join", [MyLearningController::class, 'joinClass'])->name('mylearning.join')->middleware('auth');
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+/**
+ * Admin Routes
+ */
+Route::middleware('auth')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function ()
+    {
+        Route::get('/dasboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+    }
+);
+
+/**
+ * Auth Lib Routes
+ */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('/courses', CourseController::class);
-    Route::resource('/chapters', ChapterController::class);
-    Route::resource('/topics', TopicController::class);
-    Route::resource('/lessons', LessonController::class);
-    Route::resource('/lesson-videos', LessonVideoController::class);
-    Route::resource('/lesson-pdfs', LessonPdfController::class);
-    Route::resource('/lesson-articles', LessonArticleController::class);
-    Route::resource('/practices', PracticeController::class);
-    Route::resource('/students', StudentController::class);
-    Route::resource('/course-students', CourseStudentController::class);
-    Route::resource('/instructors', InstructorController::class);
 });
 
 
