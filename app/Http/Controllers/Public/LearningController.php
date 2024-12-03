2de913a11ac9640df8d11e15fc5521cc55349f9e
hairunisa29 <hairunisa2909@outlook.com>
2024-12-03 11:46:21 +0700

    redirect to first content

--- app/Http/Controllers/Public/LearningController.php
+++ app/Http/Controllers/Public/LearningController.php
@@ -3,20 +3,33 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chapter;
 use App\Models\Course;
 use App\Models\Lesson;
+use App\Models\Topic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class LearningController extends Controller
 {
-    public function show($id): View
+    public function show($id)
     {
         $course = Course::with(
-            'chapters.topics'
+            'chapters.topics.lesson'
         )->findOrFail($id);
 
-        return view('public.learningpage.index', compact('course'));
+        // Find the first lesson by accessing its topic, then its chapter, and filter by course ID.
+        $firstLesson = Lesson::whereHas('topic.chapter', function ($query) use ($id) {
+            $query->where('course_id', $id);
+        })->first();
+
+        // Redirect to first content
+        if ($firstLesson->type == 'video') {
+            return Redirect::route('learning.show.video', ['courseId' => $id, 'topicId' => $firstLesson->topic_id]);
+        }
+
+        // return view('public.learningpage.index', compact('course'));
     }
 
     public function showVideo($courseId, $topicId): View
@@ -29,7 +42,7 @@ class LearningController extends Controller
             ->where('topic_id', $topicId)
             ->where('type', 'video')
             ->firstOrFail();
-            
+
         return view('public.learningpage.index', compact('course', 'lesson'));
     }
 }
