e7507d9cee5414136e1e1c9d74b81628b46f3589
AbidKhairyAK <abid.khairy.ak@gmail.com>
2024-12-03 09:57:27 +0700

    fix: weird input behavior on admin course crud page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/views/**/*.vue',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
