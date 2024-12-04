0c6761414cb8bccec63dbc4e0a130129d85fcd77
hairunisa29 <hairunisa2909@outlook.com>
2024-12-04 23:17:15 +0700

    fix sidebar sticky

--- resources/views/public/learningpage/index.blade.php
+++ resources/views/public/learningpage/index.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Learning ' . $course->title)
 
 @section('content')
-    <div class="m-4 lg:m-8 flex flex-col lg:flex-row gap-8">
-        <div class="w-full lg:w-2/6">
+    <div class="m-4 lg:m-8 flex flex-col md:flex-row gap-8">
+        <div class="w-full md:w-2/6 md:sticky md:top-28 md:max-h-[calc(100vh-98px)] md:overflow-y-auto">
             @include('public.learningpage.sidebar')
         </div>
 
