3136f929b946e330d0e980b155cc34339384aa37
hairunisa29 <hairunisa2909@outlook.com>
2024-12-04 15:50:35 +0700

    remove main

--- resources/views/public/learningpage/video.blade.php
+++ resources/views/public/learningpage/video.blade.php
@@ -1,7 +1,5 @@
-<main class="flex-1">
-    <iframe class="h-[200px] lg:h-[412px] w-full rounded-box" src="{{ $lesson->video->video_url }}"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-    </iframe>
-</main>
+<iframe class="h-[200px] lg:h-[412px] w-full rounded-box" src="{{ $lesson->video->video_url }}"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+</iframe>
