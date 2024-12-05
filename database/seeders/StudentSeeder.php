b01f77013eed42a4f1394994a69e94e6d14c74c9
hairunisa29 <hairunisa2909@outlook.com>
2024-12-05 14:12:33 +0700

    fix seeder and add default user admin, student, teacher

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -3,9 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class StudentSeeder extends Seeder
 {
@@ -14,18 +14,22 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        $userIds = User::all()->random(2)->pluck('id');
-        $occupations = ['Frontend Engineer', 'Software Engineer'];
-        $genders = ['Male', 'Female'];
-        $addresses = ['823 Oak St', '754 Pine St'];
+        $userIds = User::where(function ($query) {
+            $query->where('name', 'not like', '%Admin%')
+                  ->where('name', 'not like', '%Teacher%');
+        })->pluck('id');
+        // $occupations = ['Frontend Engineer', 'Software Engineer'];
+        // $genders = ['Male', 'Female'];
+        // $addresses = ['823 Oak St', '754 Pine St'];
+        $faker = Faker::create();
 
         foreach ($userIds as $userId) {
             DB::table('students')->insert([
                 'user_id'     => $userId,
-                'phonenumber' => '+62' . rand(8000000000, 8999999999),
-                'occupation'  => $occupations[array_rand($occupations)],
-                'gender'      => $genders[array_rand($genders)],
-                'address'     => $addresses[array_rand($addresses)],
+                'phonenumber' => $faker->phoneNumber(),
+                'occupation'  => $faker->jobTitle(),
+                'gender'      => $faker->randomElement(['male', 'female']),
+                'address'     => $faker->address(),
             ]);
         }
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,11 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -16,23 +14,38 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
         /**
          * Default user
          */
+
+        // Admin user
         User::create([
-            'name' => 'Dummy PSM',
-            'email' => 'dummy@prosigmaka.com',
+            'name' => 'Admin Prodemy',
+            'email' => 'dummy.admin@prosigmaka.com',
+            'password' => Hash::make('dummypsm'),
+        ]);
+
+        User::create([
+            'name' => 'Student Prodemy',
+            'email' => 'dummy.student@prosigmaka.com',
+            'password' => Hash::make('dummypsm'),
+        ]);
+
+        User::create([
+            'name' => 'Teacher Prodemy',
+            'email' => 'dummy.teacher@prosigmaka.com',
             'password' => Hash::make('dummypsm'),
         ]);
 
         /**
          * Generate 5 random user
          */
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 3; $i++) {
             User::create([
-                'name' => Str::random(10),
-                'email' => Str::random(10) . '@example.com',
-                'password' => Hash::make('password'),
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make('dummypsm'),
             ]);
         }
     }
