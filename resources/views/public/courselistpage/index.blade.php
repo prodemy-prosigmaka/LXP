4093e0538ac8266c2c6333d8915dd35ec12d3e29
hairunisa29 <hairunisa2909@outlook.com>
2024-12-19 10:17:19 +0700

    fix aspect ratio

--- resources/views/public/courselistpage/index.blade.php
+++ resources/views/public/courselistpage/index.blade.php
@@ -6,12 +6,12 @@
     <main class="m-8 md:m-20">
         <h1 class="md:mb-8 font-bold text-xl md:text-3xl">Choose your learning path</h1>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             @foreach ($courses as $course)
                 <a href="{{ route('courselist.detail', $course->id) }}"
                     class="card bg-base-100 shadow-xl hover:bg-primary transition duration-300">
                     <figure class="px-8 pt-8">
-                        <img src="{{ $course->image }}" alt="course-image" class="rounded-xl w-full h-36 object-cover" />
+                        <img src="{{ $course->image }}" alt="course-image" class="rounded-xl w-full aspect-video object-cover" />
                     </figure>
                     <div class="card-body">
                         <h2 class="card-title font-bold">{{ $course->title }}</h2>
--- resources/views/public/learningpage/video.blade.php
+++ resources/views/public/learningpage/video.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <iframe class="h-[200px] lg:h-[412px] w-full rounded-box" src="{{ $lesson->video->video_url }}"
+    <iframe class="aspect-video w-full rounded-box" src="{{ $lesson->video->video_url }}"
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
